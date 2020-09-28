92fb51361ffc93fdd10f6f4636906b680e3a9264
abid7 <abidshahriar7@gmail.com>
2020-09-29 03:38:31 +0600

    Updates

---
+++ precache-manifest.61cf913496e34bbac677323d1d86a2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf24c0f5258658f3283083c3acc8503",
+    "url": "/simple-photo-editor/index.html"
+  },
+  {
+    "revision": "02d49a70c5ab06b755a4",
+    "url": "/simple-photo-editor/static/css/main.a3a174e4.chunk.css"
+  },
+  {
+    "revision": "706048de0e30e249198d",
+    "url": "/simple-photo-editor/static/js/2.3d703d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-photo-editor/static/js/2.3d703d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d49a70c5ab06b755a4",
+    "url": "/simple-photo-editor/static/js/main.3dbb263c.chunk.js"
+  },
+  {
+    "revision": "c5c6edc7c4f50277176c",
+    "url": "/simple-photo-editor/static/js/runtime-main.44923051.js"
+  },
+  {
+    "revision": "417ed6dcf2b1278d6f5c6a5cacf3e815",
+    "url": "/simple-photo-editor/static/media/demopic.417ed6dc.jpg"
+  }
+]);
